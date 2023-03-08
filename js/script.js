f7f8bda7867908fdb0e1a1e837406fb60aca3221
CK Mirafuente <mcmirafuente@gmail.com>
2023-03-09 07:37:44 +0800

    fixes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
 
-menuIcon.onClick = () => {
+// document.addEventListener("DOMContentLoaded", () => {
+console.log("Hello World!");
+
+var menuIcon = document.querySelector('#menu-icon');
+var navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
   menuIcon.classList.toggle('bxs-x-circle')
   navbar.classList.toggle('active');
 };
@@ -13,14 +17,14 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop -150;
-    let height =sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height) {
+    if (top >= offset && top < offset + height) {
       navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a[href*=' +id+ ']').classList.add('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
       });
     };
   });
@@ -28,7 +32,7 @@ window.onscroll = () => {
   /*====navigation bar====*/
 
 
-  let header =document.querySelector('header');
+  let header = document.querySelector('header');
 
   header.classList.toggle('sticky', window.scrollY > 100);
 
@@ -40,22 +44,22 @@ window.onscroll = () => {
 
 
 ScrollReveal({
-   reset: true,
-   distance: '800px',
-   duration: 2000,
-   delay: 200
-  });
+  reset: true,
+  distance: '800px',
+  duration: 2000,
+  delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-  ScrollReveal().reveal('.home-content, .heading', { origin: top });
-  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: bottom });
-  ScrollReveal().reveal('.home-content h1, .about-img', { origin: left });
-  ScrollReveal().reveal('.home-content p, .about-content', {origin:right});
 
-
-  const typed =new Typed('.multiple-text', {
-    strings: ['Front end Developer', 'IT Expert'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
-  });
+const typed = new Typed('.multiple-text', {
+  strings: ['Front end Developer', 'IT Expert'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+// });
